11f5dbc212b7cd0e5bad0575d7768a633f0064bc
Juan Falconi <juan.falconi@epn.edu.ec>
2023-06-10 18:25:24 -0500

    Figura Cilindro

---
+++ src/Cilindro.java
@@ -0,0 +1,39 @@
+public class Cilindro {
+    //Atributos para la clase cilidro
+    private double radio;
+    private double altura;
+
+    //Constructores
+
+    public Cilindro(double radio, double altura) {
+        this.radio = radio;
+        this.altura = altura;
+    }
+
+    //Setters & Getters
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    //Calculos
+    public double volumenCindro(double radio, double altura){
+        return Math.PI * Math.pow(this.radio, 2) * this.altura;
+    }
+    public  double areaCilidro(double radio, double altura){
+        return (2 * Math.PI * this.radio * this.altura) + (2 * Math.PI * Math.pow(this.radio, 2));
+    }
+
+}
