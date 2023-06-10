b146f4089bac203635d116557db975c61d0f74aa
dennis <dennis.catana@epn.edu.ec>
2023-06-09 20:28:50 -0500

    Menu del programa

--- src/Menu.java
+++ src/Menu.java
@@ -1,37 +1,52 @@
 import java.util.Scanner;
 
 public class Menu {
+    static Scanner scanner = new Scanner(System.in);
+    static int opcion = 0;
     public static void main(String[] args) {
-        int opcion = 0;
-        Scanner ingreso = new Scanner(System.in);
 
-        while (opcion != 1 & opcion != 2 & opcion != 3 & opcion != 4) {
-            System.out.println("Bienvenidos\n");
-            System.out.println("Seleccione una figura y vea su volumen");
-            System.out.println("1) Cono");
-            System.out.println("2) Prisma");
-            System.out.println("3) Cilindro");
-            System.out.println("4) Cubo");
-            System.out.print("Ingrese su opción: ");
-            opcion = ingreso.nextInt();
+        while (opcion != 5) {
+            opcion=MenuPrincipal();
 
             switch (opcion) {
                 case 1:
-                    System.out.println("Ha seleccionado la opción 1");
+                    System.out.println("\nHa seleccionado la opción 1");
                     break;
                 case 2:
-                    System.out.println("Ha seleccionado la opción 2");
+                    System.out.println("\nHa seleccionado la opción 2");
                     break;
                 case 3:
-                    System.out.println("Ha seleccionado la opción 3");
+                    System.out.println("\nHa seleccionado la opción 3");
                     break;
                 case 4:
-                    System.out.println("Ha seleccionado la opción 4");
+                    System.out.println("\nHa seleccionado la opción 4");
+                    break;
+                case 5:
+                    System.out.println("Saliendo del programa...");
                     break;
                 default:
-                    System.out.println("Opción inválida\n");
-                    break;
+
             }
         }
+    }
+    public static int MenuPrincipal() {
+        System.out.println("\n\tBIENVENIDOS");
+        System.out.println("========================================");
+        System.out.println("\nSeleccione una figura y vea su volumen");
+        System.out.println("1) Cono");
+        System.out.println("2) Prisma");
+        System.out.println("3) Cilindro");
+        System.out.println("4) Cubo");
+        System.out.println("5) Salir del programa");
+        System.out.print("\n Ingrese una opción: ");
+        opcion = scanner.nextInt();
+        while (opcion < 1 || opcion > 5) {
+            System.out.println("=================================================================================");
+            System.out.println("\n¡¡ Ingrese una opción válida !!");
+            System.out.println("\n=================================================================================");
+            System.out.print("\nDígite la opción: ");
+            opcion = scanner.nextInt();
+        }
+        return opcion;
     }
 }
