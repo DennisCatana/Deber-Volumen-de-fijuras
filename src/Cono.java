06f17907ceaba71bfbdf0657b3db6ddaa00df5b3
Mateo_Mino <minora.mateo@gmail.com>
2023-06-09 23:20:24 -0500

    no message

--- src/Cono.java
+++ src/Cono.java
@@ -15,6 +15,67 @@ public class Cono {
     double Area_Cono_Total;
 
 //********************************************************************************************************************//
+    //Setters y Getters
+
+    //Volumen Cono
+    public double getVolumen_Cono() {
+        return Volumen_Cono;
+    }
+
+    public void setVolumen_Cono(double volumen_Cono) {
+        Volumen_Cono = volumen_Cono;
+    }
+
+    public double getRadio_Cono() {
+        return Radio_Cono;
+    }
+
+    public void setRadio_Cono(double radio_Cono) {
+        Radio_Cono = radio_Cono;
+    }
+
+    public double getAltura_Cono() {
+        return Altura_Cono;
+    }
+
+    public void setAltura_Cono(double altura_Cono) {
+        Altura_Cono = altura_Cono;
+    }
+
+    //Area Cono
+    public double getGeneratriz() {
+        return Generatriz;
+    }
+
+    public void setGeneratriz(double generatriz) {
+        Generatriz = generatriz;
+    }
+
+    public double getArea_Cono_Lateral() {
+        return Area_Cono_Lateral;
+    }
+
+    public void setArea_Cono_Lateral(double area_Cono_Lateral) {
+        Area_Cono_Lateral = area_Cono_Lateral;
+    }
+
+    public double getArea_Cono_Base() {
+        return Area_Cono_Base;
+    }
+
+    public void setArea_Cono_Base(double area_Cono_Base) {
+        Area_Cono_Base = area_Cono_Base;
+    }
+
+    public double getArea_Cono_Total() {
+        return Area_Cono_Total;
+    }
+
+    public void setArea_Cono_Total(double area_Cono_Total) {
+        Area_Cono_Total = area_Cono_Total;
+    }
+
+//********************************************************************************************************************//
     //Constructores con Sobrecarga
 
     //Constructor Volumen
